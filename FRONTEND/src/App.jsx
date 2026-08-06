fa7d56eebe91eea147dce1fc469847db68b3a64e
Sachin Kumar Sah <sachinsums08@gmail.com>
2026-08-07 01:45:57 +0530

    appointment

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <main className="main-content">
+        <div className="card">
+          <div className="image-container">
+            <img 
+              src="/Gemini_Generated_Image_267qoe267qoe267q.png" 
+              alt="Pandit Ji" 
+              className="pandit-image"
+            />
+          </div>
+          <h1 className="hindi-text">
+            ab tarik nikalne ke lie pandit ji bheju 
+            <img src="/laugh-emoji.png" className="inline-emoji" alt="laugh" />
+            <img src="/laugh-emoji.png" className="inline-emoji" alt="laugh" />
+          </h1>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
